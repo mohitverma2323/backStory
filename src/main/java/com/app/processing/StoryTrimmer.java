5d092353df7a83a5ed307910d6bab9b4361608a0
jayendra155 <jayendra155@hotmail.com>
2016-02-21 12:48:59 +0530

    Story Processing

---
+++ src/main/java/com/app/processing/StoryTrimmer.java
@@ -0,0 +1,42 @@
+package com.app.processing;
+
+public class StoryTrimmer {
+	private String editedStory;
+
+	public StoryTrimmer() {
+		editedStory = "";
+	}
+
+	public String getEditedStory() {
+		return editedStory;
+	}
+
+	public void finalCutForStory(String uneditedStory) {
+		uneditedStory.trim();
+		int wordCount = 0;
+		int length = uneditedStory.length();
+		for (int i = 1; i < length; i++) {
+			char c = uneditedStory.charAt(i);
+			if(c=='\t')
+				c=' ';
+			if (c != ' ')
+				editedStory += c;
+			else if (c == ' ' && uneditedStory.charAt(i - 1) == ' ')
+				continue;
+			else {
+				wordCount++;
+				editedStory+=c;
+			}
+		}
+		System.out.println(editedStory);
+		System.out.println(wordCount);
+
+	}
+
+	/*public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String story = br.readLine();
+		StoryTrimmer storyTrimmer = new StoryTrimmer();
+		storyTrimmer.finalCutForStory(story);
+	}*/
+}
