13a91a3c50b8a5cbc58d87d21f18305fb5ed9032
jayendra155 <jayendra155@hotmail.com>
2016-02-21 11:45:44 +0530

    test

--- src/main/java/com/app/controller/TestController.java
+++ src/main/java/com/app/controller/TestController.java
@@ -3,6 +3,7 @@ package com.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 //import com.app.pojo.User;
 
@@ -19,10 +20,11 @@ public class TestController {
 	
 
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public void tester()
+	public @ResponseBody String tester()
 	{
 		
 		System.out.println("Here");
        // User user = new User();
+		return "abc";
 	}
 }
