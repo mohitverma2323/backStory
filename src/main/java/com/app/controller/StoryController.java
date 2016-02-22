1146d4b3c77a40da901778e168b172ea08bd63ca
Jayendra Vikram Singh <jayendra155@hotmail.com>
2016-02-22 13:59:21 +0530

    Story Upload added

--- src/main/java/com/app/controller/StoryController.java
+++ src/main/java/com/app/controller/StoryController.java
@@ -2,6 +2,7 @@ package com.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.app.manager.StoryManager;
 import com.app.pojo.Story;
+import com.google.gson.Gson;
 
 @Controller
 public class StoryController {
@@ -19,5 +21,12 @@ public class StoryController {
 	public @ResponseBody String findStoryByTime(@RequestParam Story story){
 		Story generatedStory =storyManager.findStory(story);
 		return generatedStory.toString();
+	}
+	
+	@RequestMapping(value="",method=RequestMethod.POST)
+	public @ResponseBody String uploadStory(@RequestBody Story story){
+		storyManager.addNewStoryFromUSer(story, null);
+		Gson gson=new Gson();
+		return gson.toJson(story);
 	}
 }
--- src/main/java/com/app/manager/StoryManager.java
+++ src/main/java/com/app/manager/StoryManager.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import com.app.dao.StoryDao;
 import com.app.pojo.Story;
+import com.app.pojo.User;
 import com.app.processing.StoryTrimmer;
 import com.app.utility.Constants;
 
@@ -15,17 +16,27 @@ import com.app.utility.Constants;
 public class StoryManager {
 	@Autowired
 	private StoryDao storyDao;
-	public void processStoryForUpload(Story story)
-	{
-		StoryTrimmer storyTrimmer=new StoryTrimmer();
+
+	private void processStoryForUpload(Story story) {
+		StoryTrimmer storyTrimmer = new StoryTrimmer();
 		storyTrimmer.finalCutForStory(story.getStoryContent());
-		story.setTimeRequired((storyTrimmer.getWordCount()/Constants.WORDS_READ_BY_AVERAGE_USER_IN_A_MINUTE)+1);
+		story.setTimeRequired((storyTrimmer.getWordCount() / Constants.WORDS_READ_BY_AVERAGE_USER_IN_A_MINUTE) + 1);
 	}
-	public Story findStory(Story story){
-		List<Story> storyList=(List<Story>) storyDao.findStory(story.getTimeRequired());
-		int size=storyList.size();
-		Random randomNumber=new Random();
-		int randomStory=randomNumber.nextInt(size);
+
+	public Story findStory(Story story) {
+		List<Story> storyList = (List<Story>) storyDao.findStory(story.getTimeRequired());
+		int size = storyList.size();
+		Random randomNumber = new Random();
+		int randomStory = randomNumber.nextInt(size);
 		return storyList.get(randomStory);
+	}
+
+	public void addNewStoryFromUSer(Story story, User user) {
+		try {
+			processStoryForUpload(story);
+			storyDao.persistStory(story);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
